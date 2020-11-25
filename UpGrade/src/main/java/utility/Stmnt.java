4349737b42f4edd579e5d6223f17d2c9fa65b5f7
unknown <dbond.tech@gmail.com>
2020-11-25 01:19:10 -0600

    11/25 maven project with DB creds

---
+++ UpGrade/src/main/java/utility/Stmnt.java
@@ -0,0 +1,35 @@
+package utility;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Stmnt {
+
+	public Stmnt() {
+		
+	}
+	public static ConnFactory cf= ConnFactory.getInstance();
+	Connection conn = cf.getConnection();
+
+
+
+	 public static PreparedStatement makePrStmnt(String s) throws SQLException 
+	 {  
+		 Connection conn = cf.getConnection();
+		 PreparedStatement prstmt = conn.prepareStatement(s);
+	     return prstmt;
+	 }
+	 public static java.sql.ResultSet getResultSet(String sql) throws SQLException 
+	 {
+		 Connection conn = cf.getConnection();
+	     java.sql.Statement stmt = conn.createStatement();
+	     ResultSet rs = stmt.executeQuery(sql);
+	     return rs;
+	 }
+	  
+ }
+	 
+
+
