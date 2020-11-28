9feb58072d53049cc69dcf5b52bc5f01c1330e08
Stephanie Foshay <foshaystephanie@gmail.com>
2020-11-27 21:39:48 -0600

    changing department and supervisor fields on form to dropdowns. starting work on beans.

---
+++ UpGrade/src/main/java/com/revature/beans/Employee.java
@@ -0,0 +1,14 @@
+package com.revature.beans;
+
+public class Employee {
+	
+	private int empID;
+	private String firstName;
+	private String lastName;
+	private String userName;
+	private String password;
+	private double availableR;
+	private int supervisorID;
+	private int deptHeadID;
+
+}
