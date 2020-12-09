e18f11793f0df7ab05cd9186906bb94211b70dca
unknown <david1990m@gmail.com>
2020-12-09 12:28:23 -0700

    added exceed funds if checker, working on integrating s3 bucket

--- UpGrade/src/main/java/com/revature/controller/EmployeeFormController.java
+++ UpGrade/src/main/java/com/revature/controller/EmployeeFormController.java
@@ -7,9 +7,11 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.beans.Employee;
 import com.revature.beans.Reimbursement;
 import com.revature.dao.ReimbursementDAO;
 import com.revature.daoimpl.ReimbursementDAOImpl;
@@ -29,16 +31,19 @@ public class EmployeeFormController {
 		if(!req.getMethod().equals("POST")) {
 			return "HTML/EmpPortal/Form.html";
 		}
-		String firstName = req.getParameter("firstName");
-		String lastName = req.getParameter("lastName");
-		String department = req.getParameter("department");
+		HttpSession session = req.getSession();
+		int emp= (int) session.getAttribute("activeemp");
+		Employee empObj = eServ.getSessionEmp(emp);
+		req.getParameter("firstName");
+		req.getParameter("lastName");
+		req.getParameter("department");
 		String supervisor = req.getParameter("supervisor");
 		Integer supervisorID = Integer.valueOf(supervisor);
 		String eventName = req.getParameter("eventName");
 		String date = req.getParameter("date");
 		String time = req.getParameter("time");
 		String location = req.getParameter("location");
-		String workTimeMissed = req.getParameter("workTimeMissed");
+		req.getParameter("workTimeMissed");
 		String description = req.getParameter("description");
 		String cost = req.getParameter("cost");
 		Double costAmt = Double.valueOf(cost);
@@ -48,14 +53,23 @@ public class EmployeeFormController {
 		String attachmentEvent = req.getParameter("attachmentEvent");
 		String attachmentEmail = req.getParameter("attachmentEmail");
 		String approval = req.getParameter("approval");
-		Integer approvalType = Integer.valueOf(approval);
+		Integer.valueOf(approval);
 		
-		Reimbursement form = new Reimbursement(eServ.loginGetEmpID(),eventName, date,time,location,description,costAmt,attachmentEvent,gradingFormat,typeOfEvent,justification,attachmentEmail,
-				supervisorID, 1, true, 0, null, false, false, null);
+		boolean exceedsFunds;
+		if(empObj.getAvailableR()>costAmt) {
+			exceedsFunds = false;
+		} else {
+			exceedsFunds = true;
+		}
+		
+		
+		Reimbursement form = new Reimbursement(empObj.getEmpID(),eventName, date,time,location,description,costAmt,attachmentEvent,gradingFormat,typeOfEvent,justification,attachmentEmail,
+				supervisorID, 2, false, 0, null, exceedsFunds, false, null);
 		reDao.submitReimbursement(form);
 		
 		return "pending.change";
 	}
+	
 	
 	public static void getPending(HttpServletRequest req, HttpServletResponse res) throws JsonProcessingException, IOException {
 		List<Reimbursement>forms = new ArrayList<>();
