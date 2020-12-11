15c3a70d2fceee7f1b344db126581b066fcac9aa
unknown <david1990m@gmail.com>
2020-12-11 08:08:02 -0700

    cleaned up a code a bit

--- UpGrade/src/main/java/com/revature/servlet/UploadDownloadFileServlet.java
+++ UpGrade/src/main/java/com/revature/servlet/UploadDownloadFileServlet.java
@@ -1,16 +1,12 @@
 package com.revature.servlet;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.PrintWriter;
 import java.util.Iterator;
 import java.util.List;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -33,39 +29,8 @@ public class UploadDownloadFileServlet extends HttpServlet {
 		fileFactory.setRepository(filesDir);
 		this.uploader = new ServletFileUpload(fileFactory);
 	}
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		String fileName = request.getParameter("fileName");
-		if (fileName == null || fileName.equals("")) {
-			throw new ServletException("File Name can't be null or empty");
-		}
-		File file = new File(request.getServletContext().getAttribute("FILES_DIR") + File.separator + fileName);
-		if (!file.exists()) {
-			throw new ServletException("File doesn't exists on server.");
-		}
-		System.out.println("File location on server::" + file.getAbsolutePath());
-		ServletContext ctx = getServletContext();
-		InputStream fis = new FileInputStream(file);
-		String mimeType = ctx.getMimeType(file.getAbsolutePath());
-		response.setContentType(mimeType != null ? mimeType : "application/octet-stream");
-		response.setContentLength((int) file.length());
-		response.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
-
-		ServletOutputStream os = response.getOutputStream();
-		byte[] bufferData = new byte[1024];
-		int read = 0;
-		while ((read = fis.read(bufferData)) != -1) {
-			os.write(bufferData, 0, read);
-		}
-		os.flush();
-		os.close();
-		fis.close();
-		System.out.println("File downloaded at client successfully");
-	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		if (!ServletFileUpload.isMultipartContent(request)) {
 			throw new ServletException("Content type is not multipart/form-data");
 		}
